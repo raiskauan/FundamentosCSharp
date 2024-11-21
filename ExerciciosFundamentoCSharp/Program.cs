d3ab70ed7eacae19e7c75c407bcb956b0f792b4a
raiskauan <raiscesar5@gmail.com>
2024-11-21 11:40:09 -0300

    Nível 1 Exercicio 5

--- ExerciciosFundamentoCSharp/Program.cs
+++ ExerciciosFundamentoCSharp/Program.cs
@@ -1,26 +1,31 @@
-﻿namespace ExerciciosFundamentoCSharp 
+﻿using System.Text.RegularExpressions;
+
+namespace ExerciciosFundamentoCSharp 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Crie um programa em que o usuário digita uma ou mais palavras
-            //e é exibido a quantidade de caracteres que a palavra inserida tem.
+            //1. Crie um programa em que o usuário precisa digitar a placa de um veículo e o programa verifica se a placa é válida, seguindo o padrão brasileiro válido até 2018:
+            //- A placa deve ter 7 caracteres alfanuméricos;
+            //- Os três primeiros caracteres são letras (maiúsculas ou minúsculas);
+            //- Os quatro últimos caracteres são números;
+    
+            //Ao final, o programa deve exibir ***Verdadeiro*** se a placa for válida e ***Falso*** caso contrário.
 
-            Console.Write("Digite uma palavra ou mais: ");
-            string palavra = Console.ReadLine();
-            double contador = 0;
+            Console.Write("Digite a placa do carro: ");
+            string placa = Console.ReadLine();
+            string padrao = @"^[a-zA-Z]{3}\d{4}$";
+            ;
 
-            foreach (char c in palavra)
+            if (Regex.IsMatch(placa, padrao))
             {
-                if (c != ' ')
-                {
-                    contador++;
-                }
+                Console.WriteLine("Placa Valida");
             }
-                
-
-            Console.WriteLine($"Existe {contador} caracteres nessa(s) palavras");
+            else
+            {
+                Console.WriteLine("Placa Invalida");
+            }
         }
     }
 }
