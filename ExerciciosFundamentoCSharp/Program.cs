85719655338f59d5f619c41fb159ecd4d4b0ce39
raiskauan <raiscesar5@gmail.com>
2024-11-21 03:26:08 -0300

    Nível 1 Exercicio 1

---
+++ ExerciciosFundamentoCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExerciciosFundamentoCSharp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas personalizada
+            //com o nome dele é exibida:  Olá, Welisson! Seja muito bem-vindo!
+            
+            Console.Write("Digite o seu Nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+        }
+    }
+}
